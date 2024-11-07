6658aaf97b562f2472bb84fa62529cb83f5aae68
Wetter42 <62986517+Wetter42@users.noreply.github.com>
2024-11-07 05:46:59 -0800

    Update server.lua
    Made joaat function take case sensitivity into account

--- server.lua
+++ server.lua
@@ -6,12 +6,11 @@ joaathashes = {}
 
 for k, v in pairs(files2joaat) do
         if joaat(v) > 0 then
-                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v)))
-                --print(v .. " - " .. "0x" .. string.upper(string.format("%08x", joaat(v))))
+                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v, true)))
                 joaathashes[k] = finjoaat
 
         elseif joaat(v) < 0 then
-                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v) - (-2 ^ 32))) --This line basically twos-compliments the result if it's below 0;
+                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v, true) - (-2 ^ 32))) --This line basically twos-compliments the result if it's below 0;
                 joaathashes[k] = finjoaat
         end
 end
