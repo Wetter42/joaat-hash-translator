0ed481fcf90cad9ed4e199304d7417e2a5d30e80
Wetter42 <62986517+Wetter42@users.noreply.github.com>
2024-11-07 05:40:57 -0800

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,23 @@
+local filename = "path/to/inputfile"
+local outputname = '/path/to/outputfile'
+local found = false
+
+joaathashes = {}
+
+for k, v in pairs(files2joaat) do
+        if joaat(v) > 0 then
+                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v)))
+                --print(v .. " - " .. "0x" .. string.upper(string.format("%08x", joaat(v))))
+                joaathashes[k] = finjoaat
+
+        elseif joaat(v) < 0 then
+                finjoaat = "0x" .. string.upper(string.format("%08x", joaat(v) - (-2 ^ 32))) --This line basically twos-compliments the result if it's below 0;
+                joaathashes[k] = finjoaat
+        end
+end
+
+
+
+
+
+replxml(filename, outputname, blocksize)
